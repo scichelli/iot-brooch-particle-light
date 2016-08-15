8a63dab6be6936408ce6dfde5f897ff65e3588b9
Sharon Cichelli <scichelli@users.noreply.github.com>
2016-08-14 20:55:47 -0500

    Create public-facing function; control onboard LED; publish event for logging
    Details in PR #1

--- src/twitter-rgb.ino
+++ src/twitter-rgb.ino
@@ -1,7 +1,24 @@
 void setup() {
-
+    RGB.control(true);
+    RGB.brightness(0);
+    
+    Particle.function("tweet", tweetToLight);
 }
 
 void loop() {
 
+}
+
+int tweetToLight(String tweet) {
+    Particle.publish("liskov-tweet", tweet);
+    
+    tweet.toLowerCase();
+    if (tweet.indexOf("#lightup") > -1) {
+        RGB.brightness(255);
+        RGB.color(255, 0, 0);
+        delay(1000);
+    }
+    
+    RGB.brightness(0);
+    return 0;
 }
