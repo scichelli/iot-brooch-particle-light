3bfe7f9a7674bd084a88a0c5ffee6e2f5d0a4358
Sharon Cichelli <scichelli@users.noreply.github.com>
2016-10-30 16:27:03 -0500

    Expose function to call via HTTP

--- src/twitter-rgb.ino
+++ src/twitter-rgb.ino
@@ -2,23 +2,18 @@ void setup() {
     RGB.control(true);
     RGB.brightness(0);
     
-    Particle.function("tweet", tweetToLight);
-    Particle.subscribe("hook-response/tweet-parse", respondToTweet, MY_DEVICES);
+    Particle.function("setLed", setLed);
 }
 
 void loop() {
 
 }
 
-int tweetToLight(String tweet) {
-    Particle.publish("tweet-parse", tweet, PRIVATE);
-    return 0;
-}
-
-void respondToTweet(const char *event, const char *data) {
+int setLed(String colors) {
     RGB.brightness(255);
     RGB.color(255, 0, 0);
     delay(1000);
 
     RGB.brightness(0);
+    return 0;
 }
